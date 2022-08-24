8d94d9865b4e20fadfefa10bd76a0b8a09177c0e
paulbarron85 <paulnbarron@gmail.com>
2022-08-24 16:40:01 -0400

    Add in function for checking status of Machine Motion Estop and add potential python script for homing the machine motion.

--- KI_VentionRangeExtender.py
+++ KI_VentionRangeExtender.py
@@ -10,25 +10,22 @@ homed = False
 
 while true:
 	try:
+	        time.sleep(0.25)
 
-        time.sleep(0.25)
-
-        print("Socket is listening...")       
-        server.listen(1)
-        client, addr = server.accept()
-        msg = client.recv(1024)
-        print("Recieved command:", msg.decode('utf-8'))
+	        print("Socket is listening...")       
+        	server.listen(1)
+        	client, addr = server.accept()
+	        msg = client.recv(1024)
+ 		print("Recieved command:", msg.decode('utf-8'))
         
-        if msg.decode('utf-8') == "Home":
-            
-            if not homed:
-                # Home the actuator
-                print ("Axis "+ str(axis) +" is going home")
-                homed = mm.emitHome(axis)
-                print("Axis "+ str(axis) +" is at home")
+      		if msg.decode('utf-8') == "Home":
+            		if not homed:
+                		print ("Axis "+ str(axis) +" is going home")
+                		homed = mm.emitHome(axis)
+                		print("Axis "+ str(axis) +" is at home")
         
-            print("Sending HomeDone")
-            client.send("HomeDone".encode("utf-8"))
+            		print("Sending HomeDone")
+            		client.send("HomeDone".encode("utf-8"))
     
 	except:
     		print("Closing socket due to error")
