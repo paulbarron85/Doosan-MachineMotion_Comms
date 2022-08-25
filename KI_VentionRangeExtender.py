a702cd2cabf93fbb1f7580bbde127c7b282052d3
paulbarron85 <paulnbarron@gmail.com>
2022-08-25 12:08:23 -0400

    Got most parts of the communication working and delay time is much less than previous although not as good as just using the wait for motion to complete. Reduced the range extender script so that it is only doing the homing.

--- KI_VentionRangeExtender.py
+++ KI_VentionRangeExtender.py
@@ -2,37 +2,41 @@ import sys, time, socket
 from MachineMotion import *
 sys.path.append("../..")
 
+###### CONFIGURING MACHINEMOTION ######
+mm = MachineMotionV2OneDrive()
+
 server = socket.socket()
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind(('192.168.127.120', 1234))
 
+# Configure actuator
+axis = 1
+
 homed = False
 
-while true:
-	try:
-	        time.sleep(0.25)
-
-	        print("Socket is listening...")       
-        	server.listen(1)
-        	client, addr = server.accept()
-	        msg = client.recv(1024)
- 		print("Recieved command:", msg.decode('utf-8'))
-        
-      		if msg.decode('utf-8') == "Home":
-            		if not homed:
-                		print ("Axis "+ str(axis) +" is going home")
-                		homed = mm.emitHome(axis)
-                		print("Axis "+ str(axis) +" is at home")
-        
-            		print("Sending HomeDone")
-            		client.send("HomeDone".encode("utf-8"))
+while True:
+    try:
+        time.sleep(0.5)
     
-	except:
-    		print("Closing socket due to error")
-    		client.close()
-    		server.close()
-
-	finally:    
-    		print("Finally closing socket due to error")
-    		client.close()
-    		server.close()
+        print("Socket is listening...")       
+        server.listen(1)
+        client, addr = server.accept()
+        msg = client.recv(1024)
+        print("Recieved command:", msg.decode('utf-8'))
+        
+        if msg.decode('utf-8') == "Home":
+            
+            if not homed:
+                # Home the actuator
+                print ("Axis "+ str(axis) +" is going home")
+                homed = mm.moveToHome(axis)
+                mm.waitForMotionCompletion()
+                print("Axis "+ str(axis) +" homing routine complete")
+        
+            print("Sending HomeDone")
+            client.send("HomeDone".encode("utf-8"))
+        
+    except:
+        print("Closing socket due to error")
+        client.close()
+        server.close()
